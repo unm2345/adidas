acd177f5a5c0f2f55205c5005577d0a0374a837e
unm2345 <unm1004@naver.com>
2022-06-04 21:12:30 +0900

    22.06.04

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
   const splitText = (ele)=>{
     const textAry = ele.innerText.split("");
-    let html = "<div class='split-chars'>";
+    let html = "<div class='split-chars' style='display: inline-block'>";
     textAry.forEach(function(ele){
       html+= "<div class='split-char' style='display: inline-block'>" + ele + "</div>";
     })
@@ -17,9 +17,9 @@ $(function(){
     header: function(){
       $(window).scroll(function(){
         if(0 < $(this).scrollTop()){
-          $(".header").addClass("on");
+          $(".header").addClass("scroll");
         } else {
-          $(".header").removeClass("on");
+          $(".header").removeClass("scroll");
         }
       })
 
@@ -43,54 +43,41 @@ $(function(){
       })
 
       $(".js-menu_bg").click(function(){
-        $(".js-menu").removeClass("on");
-        $(".js-menu_bg").removeClass("on");
-        $("body").removeClass("hide");
-      })
-    },
-    common: function(){
-      gsap.utils.toArray(".sc_title").forEach(function(ele){
-        gsap.set(ele, {yPercent : 100, opacity: 0})
-        gsap.to(ele, {
-          scrollTrigger: {
-            trigger : ele,
-            start: "top 70%",
-          },
-          duration: 0.3,
-          opacity: 1,
-          yPercent: 0
-        })
+        $(".js-btn_close-menu").trigger("click");
       })
     },
     goods: function(){
-      gsap.set(".goods .js-item", {yPercent : 100, opacity: 0})
-      gsap.to(".goods .js-item", {
-        scrollTrigger: {
-          trigger : ".goods",
-          start: "top center",
-        },
-        stagger: 0.15,
-        opacity: 1,
-        yPercent: 0
-      })
+      gsap.set("#goods .js-sc_title", {opacity: 0, yPercent: 100});
+      gsap.set("#goods .js-item", {opacity: 0, yPercent: 100});
+      const tl = gsap.timeline({
+        scrollTrigger:{
+          trigger: "#goods",
+          start: "top center"
+        }
+      });
+
+      tl.to("#goods .js-sc_title", {opacity: 1, yPercent: 0})
+      .to("#goods .js-item", {opacity: 1, yPercent: 0, stagger: 0.1});
     },
     adiclub: function(){ 
+      gsap.set(".js-adiclub_title", {opacity: 0, yPercent: 100});
       gsap.set(".js-adiclub_word", {opacity: 0})
-      gsap.to(".js-adiclub_word", {
+
+      const tl = gsap.timeline({
         scrollTrigger: {
-          trigger : ".adiclub",
+          trigger: "#adiclub",
           start: "top center",
         },
-        stagger: 0.15,
-        opacity: 1
       })
+      tl.to(".js-adiclub_title", {opacity: 1, yPercent: 0})
+      .to(".js-adiclub_word", {stagger: 0.15, opacity: 1})
 
       ScrollTrigger.matchMedia({
         "(min-width: 769px)": function() {
           gsap.utils.toArray(".js-adiclub_word").forEach(function(ele){        
             gsap.to(ele, {
               scrollTrigger: {
-                trigger: ".adiclub",
+                trigger: "#adiclub",
                 start: "top bottom",
                 end: "bottom top",
                 scrub: 1
@@ -98,14 +85,16 @@ $(function(){
               y: -(parseInt(Math.random() * 100) + 50)
             })
           });
+        },
+        "(max-width: 768px)": function() {          
+          $(".js-adiclub_video")[0].pause();
         }       
       }); 
       
       $(".js-adiclub_btn-start").click(function(e){
         const tl = gsap.timeline();
         tl.set(".js-adiclub_dummy", {opacity: 1,"z-index": 5})
-        .to(".js-adiclub_dummy", {scaleX: 150, scaleY: 150})
-        .to(".js-adiclub_dummy", {background: "#fff"})
+        .to(".js-adiclub_dummy", {scaleX: 150, scaleY: 150, background: "#fff"})
         .to(".js-adiclub_video-wrap", {opacity: 1, visibility: "visible"})
         .set(".js-adiclub_dummy", {scaleX: 1, scaleY: 1, "z-index": -1});
 
@@ -115,8 +104,7 @@ $(function(){
       $(".js-adiclub_btn-pause").click(function(){
         const tl = gsap.timeline();
         tl.set(".js-adiclub_dummy", {background: "#ee265c", "z-index": 20})
-        .to(".js-adiclub_dummy", {scaleX: 150, scaleY: 150})
-        .to(".js-adiclub_dummy", {background: "#fff"})
+        .to(".js-adiclub_dummy", {scaleX: 150, scaleY: 150, background: "#fff"})
         .set(".js-adiclub_video-wrap", {opacity: 0, visibility: "hidden"})
         .to(".js-adiclub_dummy", {opacity: 0})
         .set(".js-adiclub_dummy", {background: "#ee265c", scaleX: 1, scaleY: 1, "z-index": -1});
@@ -125,43 +113,49 @@ $(function(){
       })
     },
     runner: function(){
-      const text = $(".js-flash").get(0);
-      splitText(text);
-      gsap.set(".js-flash .split-char", {opacity: 0, yPercent: 100});
-
+      gsap.set(".js-runner_headline", {opacity: 0});
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger : ".runner",
-          start: "top center",
-        }        
+          trigger : ".js-runner_headline",
+          start: "top bottom",
+          end: "top top",
+          scrub: 5
+        }       
       })
-      tl.to(".js-runner_dummy", {yPercent: 100, duration: 0.5})
-      .to(".js-flash .split-char", {opacity: 1, yPercent: 0, stagger: 0.1, duration: 0.5})
+      tl.to(".js-runner_headline", {opacity: 1, duration: 1})
+      tl.to(".js-runner_headline", {xPercent: -100, duration: 5}, "-=1")
     },
     company: function(){
-      gsap.set(".company .js-item", {xPercent : 100, opacity: 0})
-      gsap.to(".company .js-item", {
-        scrollTrigger: {
-          trigger : ".company",
-          start: "top center",
-        },
-        stagger: 0.15,
-        opacity: 1,
-        xPercent: 0
-      })
+      gsap.set("#company .js-sc_title", {opacity: 0, yPercent: 100});
+      gsap.set("#company .js-item", {opacity: 0, xPercent: 100});
+      gsap.set("#company .js-board_link-more", {opacity: 0, yPercent: 100});
+      const tl = gsap.timeline({
+        scrollTrigger:{
+          trigger: "#company",
+          start: "top center"
+        }
+      });
+
+      tl.to("#company .js-sc_title", {opacity: 1, yPercent: 0})
+      .to("#company .js-item", {opacity: 1, xPercent: 0, stagger: 0.1})
+      .to("#company .js-board_link-more", {opacity: 1, yPercent: 0});
     },
-    support: function(){
-      gsap.set(".support .js-item", {xPercent : 100, opacity: 0})
-      gsap.to(".support .js-item", {
-        scrollTrigger: {
-          trigger : ".support",
-          start: "top center",
-        },
-        stagger: 0.15,
-        opacity: 1,
-        xPercent: 0
-      })
-      $(".support .item_btn").click(function(){
+    support: ()=>{
+      gsap.set("#support .js-sc_title", {opacity: 0, yPercent: 100});
+      gsap.set("#support .js-item", {opacity: 0, xPercent: 100});
+      gsap.set("#support .js-board_link-more", {opacity: 0, yPercent: 100});
+      const tl = gsap.timeline({
+        scrollTrigger:{
+          trigger: "#support",
+          start: "top center"
+        }
+      });
+
+      tl.to("#support .js-sc_title", {opacity: 1, yPercent: 0})
+      .to("#support .js-item", {opacity: 1, xPercent: 0, stagger: 0.1})
+      .to("#support .js-board_link-more", {opacity: 1, yPercent: 0});      
+                  
+      $("#support .item_btn").click(function(){
         $(this).parent().toggleClass("on");
         $(this).siblings(".item_detail-area").stop().slideToggle(300);        
       })
@@ -171,7 +165,6 @@ $(function(){
   motions.init();
   motions.header();
   motions.gnb();
-  motions.common();
   motions.goods();
   motions.adiclub();
   motions.runner();
